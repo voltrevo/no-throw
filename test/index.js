73abc40daa1e1a02a97735c173e1897fa40fcfa8
Andrew Morris <voltrevo@gmail.com>
2015-09-26 21:59:48 +1000

    Add tests

--- test/index.js
+++ test/index.js
@@ -3,10 +3,34 @@
 /* global describe it */
 
 var assert = require('assert');
-// var noThrow = require('../lib');
+var noThrow = require('../lib');
+
+var sqrt = function(n) {
+  if (n < 0) {
+    throw new Error('Encountered a complication.');
+  }
+
+  return Math.sqrt(n);
+};
 
 describe('no-throw', function() {
-  it('should have unit test!', function() {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  it('prevents an exception being thrown', function() {
+    assert.deepEqual(
+      noThrow(sqrt)(-1),
+      {
+        error: new Error('foo'),
+        value: undefined
+      }
+    );
+  });
+
+  it('provides a value when an exception isn\'t thrown', function() {
+    assert.deepEqual(
+      noThrow(sqrt)(100),
+      {
+        error: undefined,
+        value: 10
+      }
+    );
   });
 });
