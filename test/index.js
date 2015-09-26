5d1e9a1cad92678ae79b7c398299b02e789a8dc3
Andrew Morris <voltrevo@gmail.com>
2015-09-26 21:46:57 +1000

    Fix lint issues

--- test/index.js
+++ test/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var assert = require('assert');
-var noThrow = require('../lib');
+/* global describe it */
 
-describe('no-throw', function () {
-  it('should have unit test!', function () {
+var assert = require('assert');
+// var noThrow = require('../lib');
+
+describe('no-throw', function() {
+  it('should have unit test!', function() {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
 });
